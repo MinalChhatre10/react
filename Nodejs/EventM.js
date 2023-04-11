dc685a97f8fe8458b1cbc6302dedf5e4e6f5bd01
MinalChhatre10 <minalchhatre10@gmail.com>
2023-04-11 19:03:52 +0530

    NodeJS

--- Nodejs/EventM.js
+++ Nodejs/EventM.js
@@ -1,11 +1,12 @@
 const EventEmiter = require("events"); // EventEmiter is a class hence first letter is uppercase
-const emmiter = new EventEmiter(); // created instance of EventEmiter
+
+const Logger = require("./Logger");
+const log1 = new Logger();
 
 //register listener
-emmiter.addListener("messageLogged", function (eventArg) {
+log1.addListener("messageLogged", function (eventArg) {
   console.log("Listener called", eventArg);
 });
 //arguments - name of the event, function
 
-//raise an event
-emmiter.emit("messageLogged", { id: 1, url: "http//.." }); //making noise or produce something/ signalling that event has happend
+log1.log("Message");
--- Nodejs/Logger.js
+++ Nodejs/Logger.js
@@ -1,10 +1,15 @@
-var url = "http://mylogger.com";
+const EventEmiter = require("events");
 
-function log(message) {
-  //send http req
-  console.log(message);
+var url = "http://mylogger.com";
+class Logger extends EventEmiter {
+  log(message) {
+    //send http req
+    console.log(message);
+    //raise an event
+    this.emit("messageLogged", { id: 1, url: "http//.." }); //making noise or produce something/ signalling that event has happend
+  }
 }
 
-module.exports.log = log;
+module.exports.log = Logger;
 
 //or- module.exports= log; // direct send func instead of object
